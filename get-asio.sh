c893ae28b97d6561acb50f6ebea7fe3b560dd1fe
Marcel Nunez <mpnunez28@gmail.com>
2024-06-19 09:21:12 -0700

    Fix link in get-asio.sh

--- get-asio.sh
+++ get-asio.sh
@@ -1,7 +1,15 @@
 #! /bin/sh
 
-wget https://versaweb.dl.sourceforge.net/p...
+# Documentation link:
+# https://think-async.com/Asio/asio-1.28.0/doc/
+#
+# Tutorial video
+# https://www.youtube.com/watch?v=1t3vTEbcn5U
+
+wget https://versaweb.dl.sourceforge.net/project/asio/asio/1.28.0%20%28Stable%29/asio-1.28.0.zip
 unzip asio-1.28.0.zip
 cd asio-1.28.0
 ./configure
 make check
+
+
