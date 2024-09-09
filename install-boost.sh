a3cb8728194f857d435274496182b56839ad8376
Marcel Nunez <marcelpnunez@gmail.com>
2024-09-08 19:36:40 -0700

    Fix --prefix flag for boost

--- install-boost.sh
+++ install-boost.sh
@@ -3,6 +3,6 @@
 wget https://boostorg.jfrog.io/artifactory/main/release/1.79.0/source/boost_1_79_0.tar.gz
 tar -xzf boost_1_79_0.tar.gz
 cd boost_1_79_0
-./bootstrap.sh --prefix ../1.79.0
+./bootstrap.sh --prefix=../1.79.0
 ./b2 install
 
