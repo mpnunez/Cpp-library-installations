8809cbf8a31320d19b59d44f055961bcf703e4dc
Marcel Nunez <marcelpnunez@gmail.com>
2024-09-23 06:07:30 -0700

    Add curlpp libcurl examples

---
+++ libcurl-curlpp/curlpp-eg/curlpp-eg.cpp
@@ -0,0 +1,40 @@
+/**
+* \file
+* The most simple example.
+* 
+*/
+
+#include <curlpp/cURLpp.hpp>
+#include <curlpp/Easy.hpp>
+#include <curlpp/Options.hpp>
+
+
+using namespace curlpp::options;
+
+int main(int, char **)
+{
+	try
+	{
+		// Our request to be sent.
+		curlpp::Easy myRequest;
+
+		// Set the URL.
+		myRequest.setOpt<curlpp::options::Url>("https://ringsdb.com/api/public/card/01001");
+
+		// Send request and get a result.
+		// By default the result goes to standard output.
+		myRequest.perform();
+	}
+
+	catch(curlpp::RuntimeError & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	catch(curlpp::LogicError & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+    
+  return 0;
+}
